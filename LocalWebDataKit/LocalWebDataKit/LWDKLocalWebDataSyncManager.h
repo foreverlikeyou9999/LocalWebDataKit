8752d99ce93d7b61f74e87ce28e68370ae1e4f17
Donald Hays <donald.hays@gmail.com>
2011-08-18 16:05:22 -0600

    Stubbed out LWDKLocalWebDataSyncManager

---
+++ LocalWebDataKit/LocalWebDataKit/LWDKLocalWebDataSyncManager.h
@@ -0,0 +1,13 @@
+//
+//  LWDKLocalWebDataSyncManager.h
+//  LocalWebDataKit
+//
+//  Created by Donald Hays on 8/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface LWDKLocalWebDataSyncManager : NSObject
+
+@end
---
+++ LocalWebDataKit/LocalWebDataKit/LWDKLocalWebDataSyncManager.m
@@ -0,0 +1,23 @@
+//
+//  LWDKLocalWebDataSyncManager.m
+//  LocalWebDataKit
+//
+//  Created by Donald Hays on 8/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LWDKLocalWebDataSyncManager.h"
+
+@implementation LWDKLocalWebDataSyncManager
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
