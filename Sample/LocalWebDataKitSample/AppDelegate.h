a60571015a4e9f2bce7f54f52acc7a7a09159243
Donald Hays <donald.hays@gmail.com>
2011-08-18 15:42:47 -0600

    Added sample project

---
+++ Sample/LocalWebDataKitSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LocalWebDataKitSampleAppDelegate.h
+//  LocalWebDataKitSample
+//
+//  Created by Donald Hays on 8/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
