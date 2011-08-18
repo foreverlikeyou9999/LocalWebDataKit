95ba49f36755e2b6c5adfcde441859d01cdd3b57
Donald Hays <donald.hays@gmail.com>
2011-08-18 15:36:23 -0600

    Added skeleton of framework project

---
+++ LocalWebDataKit/LocalWebDataKit/LocalWebDataKit.h
@@ -0,0 +1,13 @@
+//
+//  LocalWebDataKit.h
+//  LocalWebDataKit
+//
+//  Created by Donald Hays on 8/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalWebDataKit : NSObject
+
+@end
---
+++ LocalWebDataKit/LocalWebDataKit/LocalWebDataKit.m
@@ -0,0 +1,23 @@
+//
+//  LocalWebDataKit.m
+//  LocalWebDataKit
+//
+//  Created by Donald Hays on 8/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LocalWebDataKit.h"
+
+@implementation LocalWebDataKit
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
