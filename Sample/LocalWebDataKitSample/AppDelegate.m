3b65b706b2bde587b581f133628bb54708c28063
Donald Hays <donald.hays@gmail.com>
2011-08-19 10:20:03 -0600

    Begin content syncing!

--- Sample/LocalWebDataKitSample/AppDelegate.m
+++ Sample/LocalWebDataKitSample/AppDelegate.m
@@ -7,13 +7,32 @@
 //
 
 #import "AppDelegate.h"
+#import "LocalWebDataKit.h"
 
 @implementation AppDelegate
 
 @synthesize window = _window;
 
+- (NSString *)storedContentPath
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+    return [paths objectAtIndex:0];
+}
+
+- (NSString *)resourceContentPath
+{
+    NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+    NSString *contentPath = [resourcePath stringByAppendingPathComponent:@"Content"];
+    return contentPath;
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [[LWDKLocalWebDataSyncManager sharedLocalWebDataSyncManager] beginSyncingWithSeedDataPath:[self resourceContentPath]
+                                                                               storedDataPath:[self storedContentPath]
+                                                                            remoteManifestURL:nil
+                                                                              refreshInterval:60 * 60];
+    
     contentViewController = [[ContentViewController alloc] init];
     
     [self.window addSubview:contentViewController.view];
@@ -24,41 +43,27 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
+
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
-     */
+
 }
 
 - (void)dealloc
