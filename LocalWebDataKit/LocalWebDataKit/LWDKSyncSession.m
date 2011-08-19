94c14d22bc67a4eee55027d8671a5eaff140b3c2
Donald Hays <donald.hays@gmail.com>
2011-08-19 12:22:17 -0600

    Stub of LWDKSyncSession

---
+++ LocalWebDataKit/LocalWebDataKit/LWDKSyncSession.m
@@ -0,0 +1,23 @@
+//
+//  LWDKSyncSession.m
+//  LocalWebDataKit
+//
+//  Created by Donald Hays on 8/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LWDKSyncSession.h"
+
+@implementation LWDKSyncSession
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
